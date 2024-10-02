f75c789318a78fb9d10d16552f686ac61ad6afa5
Mhyles <mhylesroque@gmail.com>
2024-10-01 20:29:25 -0400

    Create hangman-game.py

---
+++ hangman-game.py
@@ -0,0 +1,36 @@
+import random
+
+def hangman_game():
+    words = ['python', 'java', 'hangman', 'computer', 'program']
+    word = random.choice(words)
+    word_letters = set(word)
+    alphabet = set('abcdefghijklmnopqrstuvwxyz')
+    used_letters = set()
+    
+    lives = 6
+    
+    while len(word_letters) > 0 and lives > 0:
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
+        
+        word_list = [letter if letter in used_letters else '_' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+        
+        user_letter = input('Guess a letter: ').lower()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+            else:
+                lives = lives - 1
+                print('Letter is not in the word.')
+        elif user_letter in used_letters:
+            print('You have already used that letter. Please try again.')
+        else:
+            print('Invalid character. Please try again.')
+    
+    if lives == 0:
+        print('Sorry, you died. The word was', word)
+    else:
+        print('Congratulations! You guessed the word', word, '!!')
+
+hangman_game()
